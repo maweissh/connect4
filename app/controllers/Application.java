88ffb54bbd435de174a17385bfcec3da2fd6a503
Markus We <markus.weisshaar@gmx.de>
2014-06-01 22:46:31 +0200

    Session Spielerei mit Login, Achtung: funktioniert noch nicht richtig

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,16 +1,29 @@
 package controllers;
 
+import javax.servlet.http.HttpSession;
+
 import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Application extends Controller {
-	public static Result init(){
-		//hier fehlt die Abfrage ob login=true
-		return ok(views.html.login.render() );
+	public static Result init() {
+		// hier fehlt die Abfrage ob login=true
+		//session().remove("username");
+		
+		if (session().containsKey("username")) {
+			return ok(views.html.login.render());
+		} else {
+			return ok(views.html.index.render("Hallo "
+					+ request().getQueryString("username")));
+		}
+
 	}
-    
-    public static Result index() {
-        return ok(views.html.index.render("Hallo ihr Opfas"));
-    }
-    
+	
+	public static Result saveLogin(){
+		session().put("username", request().getQueryString("username"));
+		return ok(views.html.index.render("Hallo "
+				+ request().getQueryString("username")));
+	}
+
+
 }
