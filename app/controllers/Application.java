1e5116d25147318d246198d1228bb8d9cf08421c
Markus We <markus.weisshaar@gmx.de>
2014-06-01 16:04:57 +0200

    View: login.html erstellt + erste css Formatierungen erstellt und eingefügt

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,6 +4,10 @@ import play.mvc.Controller;
 import play.mvc.Result;
 
 public class Application extends Controller {
+	public static Result init(){
+		//hier fehlt die Abfrage ob login=true
+		return ok(views.html.login.render() );
+	}
     
     public static Result index() {
         return ok(views.html.index.render("Hallo ihr Opfas"));
