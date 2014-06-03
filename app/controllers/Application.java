485bdafb70a20ddc6aedb82fa991e4bdff224985
ma weissh <markus.weisshaar@gmx.de>
2014-06-03 09:54:14 +0200

    Passt wieder

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -5,8 +5,6 @@ import play.mvc.Result;
 
 public class Application extends Controller {
 	public static Result init() {
-		//todo: session.remove für logout button
-		//session().remove("username");
 		
 		if (!session().containsKey("username")) {
 			return ok(views.html.login.render());
@@ -17,17 +15,17 @@ public class Application extends Controller {
 	
 	public static Result saveLogin(){
 		session().put("username", request().getQueryString("username"));
-<<<<<<< HEAD
+
 		//return redirect("/");
-		return ok(views.html.index.render(request().getQueryString("username")));		
-=======
-		return redirect("\\");		
+		//return ok(views.html.index.render(request().getQueryString("username")));		
+
+		return redirect("/");		
 	}
 	
 	public static Result doLogout(){
 		session().remove("username");
 		return ok(views.html.logout.render());
->>>>>>> 5db9a8fc046f1685f149de241bba71cf71a93f9d
+
 	}
 
 	public static Result playScreen(){
