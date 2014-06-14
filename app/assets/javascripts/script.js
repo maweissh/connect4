708da0572ea1fe115e9bdc6241a94da94e459792
Markus Weißhaar <markus.weisshaar@gmx.de>
2014-06-14 17:26:55 +0200

    Dynamische Hintergrundeinfärbung im playScreen hinzugefügt

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -12,25 +12,7 @@ function wurf_in(s) {
 function click(s) {
 	webSocket.onmessage = receiveEvent;
 	webSocket.send(s);
-
-	// var requestObj = new XMLHttpRequest();
-	// requestObj.onreadystatechange = function(){
-	// if(requestObj.readyState == 4 && requestObj.status == 200){
-	// document.getElementById("f"+s)
-	// }
-	// }
-	//	
-	//	
-	// var table = document.getElementById("table_play");
-	// var row = table.rows[s];
-	// for (var j = 0; row.cells[j]; j++){
-	// if(s < 1){
-	// alert("if");
-	// document.getElementById("f"+s+j).childNodes[0].src =
-	// '/assets/images/vggelb.gif';
-	// s++;
-	// }
-	// }
+	
 };
 
 function receiveEvent(event) {
