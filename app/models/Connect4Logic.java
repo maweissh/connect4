472c7658c49438cf1e30ab90737538966a0e9d6c
Markus Weißhaar <markus.weisshaar@gmx.de>
2014-06-15 13:27:32 +0200

    Horizontale und vertikale Gewinnüberprüfung funktioniert; Diagonalen müssen noch implementiert werden; Einbindung der checkVicktory Methode in Application erforderlich.

--- app/models/Connect4Logic.java
+++ app/models/Connect4Logic.java
@@ -1,5 +1,7 @@
 package models;
 
+import java.io.IOException;
+
 public class Connect4Logic {
 	private static String[][] array= new String[7][6];
 	
@@ -16,47 +18,121 @@ public class Connect4Logic {
 	}
 	
 	public static int checkVictory(){	
-		int p1 =0;
-		int p2 =0;
 		try {
-			for (int r = 0; r < 6; r++) {
-				p1=0;
-				p2=0;
-				for (int c = 0; c < 7; c++) {
-					if (array[c][r]==null) {
-						p1=0;
-						p2=0;
-						continue;
-					} else {
-						if (array[c][r].equals("e")) {
-							if (p2>0) {
-								p2=0;
-							}
-							p1++;
-							if (p1 == 4) {
-								return 1;
-							}
-						} else {
-							if (p1>0) {
-								p1=0;
-							}
-							p2++;
-							if (p2 == 4) {
-								return 2;
-							}
-						}
-					}
-					
-				}
+			int erg;
+			//nach viererkette in der horizontalen suchen
+			erg = checkHorizon(); 
+			if (erg != 0) {
+				return erg ;
 			}
+			
+			//nach viererkette in der vertikalen suchen
+			erg = checkVertical();
+			if (erg !=0) {
+				return erg;
+			}
+			
+			//todo: Impelementierung der checkDiagonal Methoden
+			erg = checkDiagonalOne();
+			if (erg !=0) {
+				return erg;
+			}
+			
+			erg = checkDiagonalTwo();
+			if (erg !=0) {
+				return erg;
+			}
+			
 			return 0;
-		} catch (Exception e) {
+			
+		} catch (IOException e) {
 			System.out.println("problem in victory");
 			e.printStackTrace();
-		}
+			return 6;
+		}		
 		
+	}
+	
+	private static int checkHorizon() throws IOException{
+		int p1 =0;
+		int p2 =0;
+		for (int r = 0; r < 6; r++) {
+			p1=0;
+			p2=0;
+			for (int c = 0; c < 7; c++) {
+				if (array[c][r]==null) {
+					p1=0;
+					p2=0;
+					continue;
+				} else {
+					if (array[c][r].equals("e")) {
+						if (p2>0) {
+							p2=0;
+						}
+						p1++;
+						if (p1 == 4) {
+							return 1;
+						}
+					} else {
+						if (p1>0) {
+							p1=0;
+						}
+						p2++;
+						if (p2 == 4) {
+							return 2;
+						}
+					}
+				}
+				
+			}
+		}
 		return 0;
 	}
+	
+	private static int checkVertical() throws IOException{
+		int p1 =0;
+		int p2 =0;
+		for (int c = 0; c < 7; c++) {
+			p1=0;
+			p2=0;
+			for (int r = 0; r < 6; r++) {
+				if (array[c][r]==null) {
+					p1=0;
+					p2=0;
+					continue;
+				} else {
+					if (array[c][r].equals("e")) {
+						if (p2>0) {
+							p2=0;
+						}
+						p1++;
+						if (p1 == 4) {
+							return 1;
+						}
+					} else {
+						if (p1>0) {
+							p1=0;
+						}
+						p2++;
+						if (p2 == 4) {
+							return 2;
+						}
+					}
+				}
+				
+			}
+		}
+		return 0;		
+	}
+	
+	private static int checkDiagonalOne(){
+		return 0;
+	}
+	
+	private static int checkDiagonalTwo(){
+		return 0;
+	}
+	
 	
 	public static void clearArray(){
 		try {
