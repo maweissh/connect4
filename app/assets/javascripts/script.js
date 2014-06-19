21d06e887f5b42aac6e7d738f611981dc7eacf08
Stefano <Stefano89@gmx.net>
2014-06-19 14:23:21 +0200

    Mauscursor wechselt, jenachdem ob der Spieler dran ist oder nicht

--- app/assets/javascripts/script.js
+++ app/assets/javascripts/script.js
@@ -1,11 +1,17 @@
 /**
  * The Connect 4 javascript
  */
+var player = $("#player").value();
 /*
  * Play Screen
  */
 function wurf_in(s) {
-	click(s - 1);
+	if($("a").css("cursor") == "not-allowed"){
+			//do nothing
+	} else {
+		click(s - 1);
+	}
+
 };
 
 function click(s) {
@@ -20,6 +26,15 @@ function receiveEvent(event) {
 	var values = text.split(",");
 	var row = "f" + values[1] + values[2];
 		
+	// at the beginning of the game, set the cursor for player 2 to "not-allowed"
+	
+	// read the number of the player
+	if(player == "Player 2"){
+		$("a").css("cursor", "not-allowed");
+		$("#table_play").css("cursor", "not-allowed");
+	}
+	
+	// change the picture where the player had clicked
 	if (values[0] == "eins") {
 		document.getElementById(row).childNodes[0].src = '/assets/images/vggelb.gif';
 	}else{
@@ -40,23 +55,28 @@ function unhover(element, row){
 
 function blurEffect(text){
 	var color = document.getElementById("play_body_left").style.backgroundColor;
+
 	switch (text){
 	case "eins" :
 		if (color == "yellow"){
+			disableMouseCursor();
 			$("#play_body_left").css("box-shadow", "0px 0px 5px 5px white");
-			$("#play_body_right").css("box-shadow", "0px 0px 5px 5px red");
+			$("#play_body_right").css("box-shadow", "0px 0px 5px 5px black");
 		} else {
-			$("#play_body_left").css("box-shadow", "0px 0px 5px 5px red");
+			enableMouseCursor();
+			$("#play_body_left").css("box-shadow", "0px 0px 5px 5px black");
 			$("#play_body_right").css("box-shadow", "0px 0px 5px 5px white");
 		}
 		break;
 	case "zwei" :
 		if (color == "yellow"){
-			$("#play_body_left").css("box-shadow", "0px 0px 5px 5px yellow");
+			enableMouseCursor();
+			$("#play_body_left").css("box-shadow", "0px 0px 5px 5px black");
 			$("#play_body_right").css("box-shadow", "0px 0px 5px 5px white");
 		} else {
+			disableMouseCursor();
 			$("#play_body_left").css("box-shadow", "0px 0px 5px 5px white");
-			$("#play_body_right").css("box-shadow", "0px 0px 5px 5px yellow");
+			$("#play_body_right").css("box-shadow", "0px 0px 5px 5px black");
 		}
 		break;
 	}
@@ -82,18 +102,20 @@ function doDivColor(gamer){
 	//Dynamisches einfärben der player divs					
 	if(gamer == "Player 1"){
 		$("#play_body_left").css("background-color", "yellow");
-		$("#play_body_left").css("box-shadow", "0px 0px 5px 5px yellow");
+		$("#play_body_left").css("box-shadow", "0px 0px 5px 5px black");
 		$("#play_body_right").css("background-color", "red");
 	}else{
 		$("#play_body_left").css("background-color", "red");
 		$("#play_body_right").css("background-color", "yellow");
-		$("#play_body_right").css("box-shadow", "0px 0px 5px 5px yellow");
+		$("#play_body_right").css("box-shadow", "0px 0px 5px 5px black");
 	} 	
 };
 
-function disableMouseCursor(text){
-	document.body.style.cursor = not-allowed;
+function disableMouseCursor(){
+	$('#table_play').css('cursor', 'not-allowed');
+	$('a').css('cursor', 'not-allowed');
 };
 function enableMouseCursor(){
-	document.body.style.cursor = auto;
+	$('#table_play').css('cursor', 'default');
+	$('a').css('cursor', 'default');
 };
